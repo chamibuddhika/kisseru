0d1b34138bd61ba04d61ca5d3468f9ff4b602a54
chamibuddhika <chamibuddhika@gmail.com>
2018-07-10 12:25:14 -0400

    DSL

--- api/curry.py
+++ api/curry.py
@@ -15,6 +15,23 @@ if __name__ == "__main__":
                &  {'join': kiseru._bykey('') in [email, store]    \
                ^  (kiseru._check(2, '>'), iff, kiseru._abort())   \ 
                | save 
+
+    models = [LinearRegression(), RandomForest(), KNN(n_neighbors=7), SVR()]
+    run_models = ModelRunner(models).setErrorType('RSME') 
+
+    p = start> LoadNetCDF4('data.nc') | SummarizeGrid('5x5') // {'splitBy': cell} in run_models | Save('models.csv')
+    p.setName('Weather Model Selection')
+    
+    repo = PipelineRepo('rest_url')
+    repo.submitPipeline(p)
+    p = repo.getPipeline('Weather Model Selection')
+
+    p.run('spark://spark_url')       # Runs on Spark using SPARK runner
+    p.run('airavata://airavata_url') # Runs on Spark using SPARK runner
+    p.run('cluster://helix_url')     # Runs on the cluster as a Helix DAG
+    p.run()                          # Runs locally
+
+
     # b = a(2)
     # c = b(3)
 
