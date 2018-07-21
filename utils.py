64010d87b91b2f845de919c30293221f1a99cf63
chamibuddhika <chamibuddhika@gmail.com>
2018-07-21 13:48:29 -0400

    Fixed a bug caught with unit tests

--- utils.py
+++ utils.py
@@ -21,6 +21,13 @@ def remove_suffix(text, suffix):
 def get_indentation(line):
     whitespace_regex = re.compile('\s+')
     match = whitespace_regex.search(line)
+    indent = 0
     if match:
-        return len(match.group(0))
+        for c in match.group(0):
+            if c == '\t':
+                indent += 4
+            else:
+                indent += 1
+
+        return indent
     return 0
