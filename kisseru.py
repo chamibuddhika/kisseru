2299f6b97e0d995ec0f657e09e0728c0648e436d
chamibuddhika <chamibuddhika@gmail.com>
2018-07-24 19:33:15 -0400

    WIP : Task graph generation

--- kisseru.py
+++ kisseru.py
@@ -6,12 +6,9 @@ import types
 import re
 import functools
 import logging
-
 import time
 
 from enum import Enum
-from inspect import Signature
-from inspect import signature
 
 from func import parse_fn
 from func import recompile
@@ -23,6 +20,8 @@ from logger import LoggerExit
 from func import ASTOps
 from profiler import ProfilerEntry
 from profiler import ProfilerExit
+from typed import get_type
+from typed import Type
 
 _CSV = 'CSV'
 
@@ -46,7 +45,7 @@ HandlerRegistry.register_posthandler(logger_exit)
 
 
 class Types(Enum):
-    _CSV = 1
+    CSV = 1
     ESRI = 2
     GRB = 3
     NETCDF4 = 4
@@ -66,25 +65,91 @@ def process_fn(func):
     return recompile(fnIR, func)
 
 
+class Port(object):
+    def __init__(self, typ, index, task):
+        self.type = typ
+        self.index = index
+        self.task_ref = task
+
+
+class Edge(object):
+    def __init__(self):
+        self.source = None
+        self.dest = None
+
+
+class Tasklet(object):
+    def __init__(self, parent):
+        self.parent = parent
+        self.out_slot_in_parent = -1
+
+
 class Task(object):
     def __init__(self, runner, fn, args, kwargs):
-        self.runner = runner
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
+        self._runner = runner
+        self._fn = fn
+        self.args = {}
+        self.inputs = {}
+        self.outputs = {}
+        self.edges = []
+        self.name = fn.__name__
         self.id = None
+
         self._set_inputs(fn, args, kwargs)
+        self._set_outputs(fn)
 
     def _set_inputs(self, fn, args, kwargs):
-        print(*args)
-        print(kwargs)
-        print(inspect.signature(fn))
-        # ba = inspect.signature(fn).bind(*args, kwargs)
-        # ba.apply_defaults()
-        # print(ba)
+        sig = inspect.signature(fn)
+        params = sig.parameters
+        # print(params)
+
+        ba = sig.bind(*args, **kwargs)
+        ba.apply_defaults()
+        arguments = ba.arguments
+        # print(arguments)
+
+        if len(params) != len(arguments):
+            raise Exception(
+                "{} accepts {} arguments. But {} were given".format(
+                    self.name, len(params), len(arguments)))
+
+        for pname, param in params.items():
+            value = arguments[pname]
+            py_type = param.annotation
+            if py_type == inspect.Parameter.empty:
+                py_type = type(value)
+            param_type = get_type(py_type)
+
+            if isinstance(value, Task):
+                pass
+            self.args[pname] = value
+            self.inputs[pname] = Port(param_type, pname, self)
+            '''
+            if not isinstance(value, param_type):
+                if is_coerceable(value, param_type):
+                    pass
+                else:
+                    raise Exception(
+                        "Type Error: {} does not match type {}".format(
+                            str(value), str(param_type)))
+                # print("%s : %s" % (str(value), str(param_type)))
+            '''
+
+    def _set_outputs(self, fn):
+        sig = inspect.signature(fn)
+        if type(sig.return_annotation) == tuple:
+            for index, ret_type in enumerate(sig.return_annotation):
+                self.outputs[str(index)] = Port(
+                    get_type(ret_type), str(index), self)
+        else:
+            self.outputs[str(0)] = Port(
+                get_type(sig.return_annotation), str(0), self)
 
     def set_id(self, tid):
         self.id = tid
+
+    def run(self):
+        return self._runner(**self.args)
 
 
 class TaskGraph(object):
@@ -100,22 +165,17 @@ _graph = TaskGraph()
 params = {'split': None}
 
 
-def gen_runner(fn, *args, **kwargs):
-    def run_task():
+def gen_runner(fn):
+    def run_task(**kwargs):
         ctx = Context(fn)
 
-        print("Running pre handlers")
-        print(HandlerRegistry().pre_handlers)
+        log.info("Running pre handlers")
+        log.info(HandlerRegistry().pre_handlers)
         for pre in HandlerRegistry.pre_handlers:
             pre.run(ctx)
 
-        print(args)
-        print(kwargs)
-        ret = None
-        ba = signature(fn).bind(*args, **kwargs)
-        ba.apply_defaults()
         try:
-            ret = ctx.fn(*ba.args, **ba.kwargs)
+            ret = ctx.fn(**kwargs)
         except:
             # [TODO] Ideally we want to match the original line info in the
             # printed trace back for better script debuggability. We should
@@ -134,18 +194,14 @@ def gen_runner(fn, *args, **kwargs):
 
 def task(**params):
     def decorator(func):
-        sig = signature(func)
-        for p in sig.parameters:
-            print(sig.parameters[p].annotation)
-        print(sig.return_annotation)
         # new_sig = sig.replace(return_annotation=Signature.empty)
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             global _graph
-            task = Task(gen_runner(func, *args, **kwargs), func, args, kwargs)
+            task = Task(gen_runner(func), func, args, kwargs)
             _graph.add_task(task)
-            return task.runner()
+            return task.run()
 
         # wrapper.__signature__ = new_sig
         log.info("Return wrapper")
