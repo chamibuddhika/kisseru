15d4a8c0c8dca72876da1df2b29eb5c7f4d8be10
chamibuddhika <chamibuddhika@gmail.com>
2018-07-26 19:58:41 -0400

    Making logs more fancy

--- logger.py
+++ logger.py
@@ -8,8 +8,13 @@ class LoggerEntry(Handler):
 
     def run(self, ctx):
         print(Colors.OKGREEN +
-              "[KISERU] INFO - Running {}".format(ctx.get('__name__')) +
-              Colors.ENDC)
+              "[KISSERU] Running {}".format(ctx.get('__name__')) + Colors.ENDC)
+        print(Colors.OKGREEN +
+              "[KISSERU] {} inputs : ".format(ctx.get('__name__')) +
+              Colors.ENDC + Colors.OKBLUE + Colors.BOLD +
+              "{}".format(ctx.args) + Colors.ENDC)
+        print(Colors.OKGREEN + "            .             " + Colors.ENDC)
+        print(Colors.OKGREEN + "            .             " + Colors.ENDC)
 
 
 class LoggerExit(Handler):
@@ -17,5 +22,9 @@ class LoggerExit(Handler):
         Handler.__init__(self, name)
 
     def run(self, ctx):
-        print(Colors.OKGREEN + "[KISERU] INFO - {} output : \n{}".format(
-            ctx.get('__name__'), ctx.ret) + Colors.ENDC)
+        print(Colors.OKGREEN +
+              "[KISSERU] {} output : ".format(ctx.get('__name__')) +
+              Colors.ENDC + Colors.OKBLUE + Colors.BOLD +
+              "{}".format(ctx.ret) + Colors.ENDC)
+        print("========================================")
+        print("                  \/                    \n")
