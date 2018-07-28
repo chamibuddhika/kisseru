9ab756fa62a3f0d0e9d78d186584e9b4d8752b94
chamibuddhika <chamibuddhika@gmail.com>
2018-07-27 20:15:47 -0400

    Add couple of working examples which tests basic pipeline definition, data transformation

--- add.py
+++ add.py
@@ -1,14 +1,46 @@
+import time
+
 from kisseru import task
-from kisseru import _CSV
+from kisseru import app
+from kisseru import AppRunner
 
 
-@task(split='dfd')
-def add(a: int, b: int, c: int, name='add') -> _CSV:
-    b = a + c \
-            + a
-    '''bash ls -al > %{b}.txt'''
-    return d
+@task()
+def add_and_save(a, b, c) -> int:
+    time.sleep(2)
+    d = a + b + c
+    '''bash 
+        echo %{d} > result.txt
+        %{outfile} = result.txt
+        '''
+    return outfile
+
+
+@task()
+def add1(infile) -> int:
+    time.sleep(3)
+    '''bash 
+        %{value} = `cat %{infile}`
+        '''
+    return int(value) + 1
+
+
+@task()
+def sub(a, infile) -> int:
+    time.sleep(4)
+    '''bash
+    %{value} = `cat %{infile}`
+    '''
+    return a - int(value)
+
+
+@app()
+def myapp():
+    a = add_and_save(1, 2, 3)
+    b = add1(a)
+    c = sub(b, a)
 
 
 if __name__ == "__main__":
-    print(add(1, 2, 3, name='add'))
+    ar = AppRunner(myapp)
+    ar.run()
