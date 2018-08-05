4129e8fdf3664daace6b480f80173624d852e898
chamibuddhika <chamibuddhika@gmail.com>
2018-08-05 14:18:53 -0400

    Added 'briefer' long description

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+long_description = """
+Kisseru lets you script your data pipelines. No need to learn another framework or another DSL. Just write pipeline tasks as plain python functions. And then chain the function calls the way you need the data to flow in the pipeline and you are done!!
+
+You can take the pipeline and run it locally in your machine (or in a cluster or in a cloud if you want it to scale -- these modes still in the works) and Kisseru will figure out how to run it most efficiently without you having to worry
+about it."""
 
 setuptools.setup(
     name="kisseru",
     version="0.1",
     author="Buddhika Chamith",
     author_email="chamibuddhika@gmail.com",
-    description=
-    "A simply scriptable workflow library for application pipelines.",
+    description="A simply scriptable workflow library for data pipelines.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/chamibuddhika/kisseru",
