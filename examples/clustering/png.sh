e293444c44e448273b5b6236ef123c61618f35fe
chamibuddhika <chamibuddhika@gmail.com>
2018-08-06 22:05:09 -0400

    Making png generation script work for other oses

--- examples/clustering/png.sh
+++ examples/clustering/png.sh
@@ -1,4 +1,21 @@
 dot -Tpng -O cluster_app-before.dot
 dot -Tpng -O cluster_app-after.dot
-open cluster_app-before.dot.png
-open cluster_app-after.dot.png
+
+platform='unknown'
+case "$OSTYPE" in
+  darwin*)  platform='osx' ;; 
+  linux*)   platform='linux' ;;
+  cygwin*)  platform='cygwin' ;;
+  *)        platform='linux' ;;
+esac   
+
+if [[ $platform == 'osx' ]]; then
+   open cluster_app-before.dot.png
+   open cluster_app-after.dot.png
+elif [[ $platform == 'linux' ]]; then
+   xdg-open cluster_app-before.dot.png
+   xdg-open cluster_app-after.dot.png
+elif [[ $platform == 'cygwin' ]]; then
+   cygstart cluster_app-before.dot.png
+   cygstart cluster_app-after.dot.png
+fi
