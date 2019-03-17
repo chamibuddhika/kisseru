250ddefb50bc84a391b395bd98176829316c4075
Buddhika <chamibuddhika@gmail.com>
2019-03-17 18:15:00 +0530

    Fix import error in pip package.

--- kisseru/__init__.py
+++ kisseru/__init__.py
@@ -1,1 +1,13 @@
 name = "kisseru"
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from .kisseru import csv
+from .kisseru import xls
+from .kisseru import png
+from .kisseru import task
+from .kisseru import app
+from .kisseru import AppRunner
