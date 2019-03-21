b0d7a474b83c4627e3a9cc589d595ffa37859516
Buddhika <chamibuddhika@gmail.com>
2019-03-21 17:00:02 +0530

    Add missing exports to the global exports.

--- kisseru/__init__.py
+++ kisseru/__init__.py
@@ -5,9 +5,12 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from .kisseru import csv
-from .kisseru import xls
-from .kisseru import png
-from .kisseru import task
+from .backend import BackendType
+from .backend import BackendConfig
+from .backend import Backend
 from .kisseru import app
 from .kisseru import AppRunner
+from .kisseru import csv
+from .kisseru import png
+from .kisseru import task
+from .kisseru import xls
